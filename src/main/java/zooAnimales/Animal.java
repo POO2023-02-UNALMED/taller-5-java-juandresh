811a5941d22c8d48a69635a64626be636f0b5986
juandresh <141792824+juandresh@users.noreply.github.com>
2023-09-18 17:23:48 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -21,7 +21,7 @@ public class Animal {
 	}
 	
 	public static String totalPorTipo() {
-		return("Mamiferos: "+Mamifero.cantidadMamiferos()+"\n"+"Aves: "+Aves.cantidadAves()+"\n"+"Peces: "+Pez.cantidadPeces()+"\n"+"Reptiles: "+Reptil.cantidadReptiles()+"\n"+"Anfibios: "+Anfibio.cantidadAnfibios()+"\n");
+		return("Mamiferos: "+Mamifero.cantidadMamiferos()+"\n"+"Aves: "+Ave.cantidadAves()+"\n"+"Peces: "+Pez.cantidadPeces()+"\n"+"Reptiles: "+Reptil.cantidadReptiles()+"\n"+"Anfibios: "+Anfibio.cantidadAnfibios()+"\n");
 	}
 	
 	public String toString() {
